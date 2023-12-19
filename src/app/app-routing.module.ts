6abc01c4dbe51b9bb1fe453c05380253ed73ed96
MilanChudasma <chudasmamilan610@gmail.com>
2023-12-19 17:44:32 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginpageComponent } from './comp/loginpage/loginpage.component';
+import { FormsComponent } from './comp/forms/forms.component';
+import { PagenotfoundpageComponent } from './comp/pagenotfoundpage/pagenotfoundpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginpageComponent },
+  { path: 'form', component: FormsComponent },
+  { path: '',   redirectTo: '/login', pathMatch: 'full' }, // redirect to `first-component`
+  { path: '**', component: PagenotfoundpageComponent },  // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginpageComponent } from './comp/loginpage/loginpage.component';
+import { HeaderComponent } from './comp/header/header.component';
+import { FooterComponent } from './comp/footer/footer.component';
+import { FormsComponent } from './comp/forms/forms.component';
+import { PagenotfoundpageComponent } from './comp/pagenotfoundpage/pagenotfoundpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginpageComponent,
+    HeaderComponent,
+    FooterComponent,
+    FormsComponent,
+    PagenotfoundpageComponent
   ],
   imports: [
     BrowserModule,
